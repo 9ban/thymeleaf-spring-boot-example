312da938e79b062813c801760158164194da987c
liyi <liyi.9ban@gmail.com>
2016-02-26 14:26:01 +0800

    expression

---
+++ expression/src/main/java/com/nban/expression/ExpressionApplication.java
@@ -0,0 +1,12 @@
+package com.nban.expression;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExpressionApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ExpressionApplication.class, args);
+	}
+}
