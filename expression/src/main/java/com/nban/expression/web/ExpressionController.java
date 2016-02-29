5128e6c05744a2f3d92eb934b357d3b09f573584
liyi <liyi.9ban@gmail.com>
2016-02-29 16:25:59 +0800

    expression

--- expression/src/main/java/com/nban/expression/web/ExpressionController.java
+++ expression/src/main/java/com/nban/expression/web/ExpressionController.java
@@ -1,9 +1,11 @@
 package com.nban.expression.web;
 
-import com.fasterxml.jackson.databind.util.JSONPObject;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import javax.servlet.http.HttpServletRequest;
+import java.util.Date;
 
 /**
  * Created by liyi on 2016/2/26.
@@ -14,7 +16,9 @@ public class ExpressionController {
 
     @RequestMapping(value="index")
     public String index(Model model){
-        model.addAttribute("li","var");
+        model.addAttribute("li","I'm var");
+        model.addAttribute("current",new Date());
         return "expression";
     }
+
 }
